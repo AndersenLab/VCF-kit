3684cfc8695a6fc7e740f36ab9f8576d3fbf94d0
Daniel Cook <danielecook@gmail.com>
2014-07-23 14:02:26 -0500

    Adding Helper Functions

---
+++ helper_fcns.R
@@ -0,0 +1,13 @@
+library(stringr)
+library(VennDiagram)
+library(ggplot2)
+
+
+
+import_table <- function(t_name, f) { 
+  # This function can import data from bcftools stats function
+  t <- read.csv2(pipe(sprintf("egrep '^(# )%s[^,]|^%s' %s", t_name, t_name, f)), as.is=T, sep='\t',header=T, check.names=FALSE)
+  names(t) <- lapply(names(t), function(x) make.names(gsub("\\[[1-9]+\\]","",x)))
+  # Remove first column and return table.
+  t[,c(-1)]
+}
