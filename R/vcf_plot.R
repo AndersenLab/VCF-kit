bb68fd27415cf5599dfa81586bb26ef34415e830
Daniel Cook <danielecook@gmail.com>
2014-08-07 15:26:20 -0500

    Querying samples/variants functions merged.

--- R/vcf_plot.R
+++ R/vcf_plot.R
@@ -8,82 +8,79 @@ library(ggplot2)
 
 setwd("~/Documents/git/vcf-toolbox/")
 
-vcf <- "00_ALL_m.filter.lcr.bcf"
-
-
+vcf <- "IND_BGI1-RET2-AB4-5cbd4-90f54.bam.het.bcf"
 var <- c("DP", "QUAL")
-
-#==================================================================#
-# Get Sample Data - Retrieves variables for all samples in a vcf   #
-#==================================================================#
-get_sample_data <- function(vcf, var, fast_sample=F) {
-  # Retrieve data for a sample variable.
-  var_opts <- list(depth="%DP", dp="%DP")
-  
-  # Take sample rather than all data.
-  if (fast_sample == T) {
-    fast_sample <- "| awk 'NR % 200 == 0'"
-  } else {
-    fast_sample <- ""
-  }
-  
-  # Set the variable for pre-defined terms, otherwise use user input.
-  for (x in range(1,length(var))) {
-    print(x)
-    if (is.null(var_opts[[tolower(var[x])]]) == F) {
-      var[x] <- var_opts[[tolower(var[x])]]
-    }
-  }
-  
-    
-  # Function for returning depth for every sample
-  tmp <- tempfile()
-  # Get sample names
-  samples <- read.table(pipe(sprintf("bcftools query -l %s", vcf)), col.names=c("sample"), stringsAsFactors=F )
-  system(sprintf("bcftools query -f '%%CHROM\t%%POS[\t%s]\n' %s %s > %s", var, vcf, fast_sample, tmp))
-  r <- fread(tmp,header=F)
-  setnames(r,c("CHROM", "POS", samples$sample))
-  r
-}
-
 
 #==================================================================#
 # Get Variant Data - Retrieves variables for all variants in a vcf #
 #==================================================================#
-get_variant_data <- function( vcf, var, fast_sample = F, no_multi = T) {
-  var_names <- c("CHROM","POS",make.names(gsub("%","",var)))
-  var# Retrieve data for a variant variable.
+get_variant_data <- function( vcf, info_var, sample_var=NA, fast_sample = F, no_multi = T) {
+  
+  # Set up Variable Names
+  q_vars <- c(info_var, sample_var)
+  q_vars <- q_vars[!is.na(q_vars)]
+  var_names <- c("CHROM","POS",make.names(gsub("%","",q_vars)))
+  
   var_opts <- list(depth="%DP", "quality"="%QUAL", qual="%QUAL", "%qual" = "%QUAL", "%dp"="%DP", "dp"= "%DP")
   
+  # Process Variant (INFO) Variables
+  
   # Set the variable for pre-defined terms, otherwise use user input.
-  for (x in seq(1,length(var))) {
-    if (is.null(var_opts[[tolower(var[x])]]) == F) {
-      var[x] <- var_opts[[tolower(var[x])]]
+  for (x in seq(1,length(info_var))) {
+    if (is.null(var_opts[[tolower(info_var[x])]]) == F) {
+      info_var[x] <- var_opts[[tolower(info_var[x])]]
     }
   }
   
-  # Sample 1/200 / For quick sample.
+  
+  # Process Sample Variables
+  var_opts <- list(depth="%DP", dp="%DP")
+  for (x in range(1,length(var))) {
+    if (is.null(var_opts[[tolower(sample_var[x])]]) == F) {
+      sample_var[x] <- var_opts[[tolower(sample_var[x])]]
+    }
+  }
+  
+  if (!is.na(sample_var)) {
+    sample_var <- sprintf("[\t%s]", sample_var)
+  } else {
+    sample_var <- ""
+  }
+  
+  # Get Contigs
+  # Fast sample - only take beginning of chromosomes.
   if (fast_sample == T) {
-    fast_sample <- "| awk 'NR % 200 == 0'"
+    # Get contigs
+    tmp <- tempfile()
+    system(sprintf("bcftools view %s | head -n 500 | grep '##contig' > %s", vcf, tmp), wait=T)
+    contigs <- gsub("(##contig=<ID=|,.*)","",readLines(tmp))
+    fast_sample <- paste("--regions ",sprintf("%s", paste0(contigs,":1-1000000", collapse=",")), collapse=" ")
   } else {
     fast_sample <- ""
   }
   
   # Filter multiallelic variants
   if (no_multi == T) {
-    no_multi = sprintf("bcftools view -O b -M 2 %s |", vcf)
+    # If fast sampling - put here, otherwise, put later.
+    no_multi = sprintf("bcftools view -O b -M 2 %s %s |", vcf, fast_sample)
+    fast_sample <- ""
     vcf <- ""
   } else {
     no_multi <- ""
   }
   
   tmp <- tempfile()
-  system(sprintf("%s bcftools query -f '%%CHROM\t%%POS%s\n' %s %s > %s",no_multi, paste0("\t", var, collapse=""), vcf, fast_sample, tmp))
+  system(sprintf("%s bcftools query %s -f '%%CHROM\t%%POS%s%s\n' %s > %s",no_multi, fast_sample, paste0("\t", info_var, collapse=""), sample_var, vcf, tmp))
+  print(sprintf("%s bcftools query %s -f '%%CHROM\t%%POS%s%s\n' %s > %s",no_multi, fast_sample, paste0("\t", info_var, collapse=""), sample_var, vcf, tmp))
   r <- fread(tmp)
   setnames(r, var_names)  
   # Rename Variables  
   r
 }
+
+
+r <- get_variant_data(vcf, info_var = c("%DP", "quality"), sample_var = "%TGT", fast_sample=T)
+
 
 
 list_variant_variables <- function( vcf ) {
@@ -95,22 +92,20 @@ genetic_scale <- function(n) {
 }
 
 plot_variant_data <- function( vcf, var, fast_sample = F, scale = "equal") {
-    representation <- list(equal = "fixed", actual = "free_x")
-    r <- get_variant_data(vcf = vcf, var = var, fast_sample = fast_sample)
-    ggplot(r) +
-      geom_point( aes(x=V2,y=V3, alpha=0.2)) +
-      facet_grid(~V1, scales="free_x", space=representation[[scale]], margins=F) +
-      theme_bw() +
-      labs(x="Chromosome", y = var) +
-      theme(legend.position="none", panel.grid.major = element_line(colour = "#4c4c4c", linetype = "dotted")) +
-      stat_smooth( aes(x=V2,y=V3), fill = "grey50", size=2, color="#0080ff") +
-      scale_x_continuous(labels = genetic_scale)
+  representation <- list(equal = "fixed", actual = "free_x")
+  r <- get_variant_data(vcf = vcf, var = var, fast_sample = fast_sample)
+  ggplot(r) +
+    geom_point( aes(x=V2,y=V3, alpha=0.2)) +
+    facet_grid(~V1, scales="free_x", space=representation[[scale]], margins=F) +
+    theme_bw() +
+    labs(x="Chromosome", y = var) +
+    theme(legend.position="none", panel.grid.major = element_line(colour = "#4c4c4c", linetype = "dotted")) +
+    stat_smooth( aes(x=V2,y=V3), fill = "grey50", size=2, color="#0080ff") +
+    scale_x_continuous(labels = genetic_scale)
 }
 
 
 
-r <- get_variant_data(vcf, var = c("%DP", "quality", "%AC"))
-r2 <- get_variant_data("00_ALL_c.bcf", var = "%DP")
 
 plot_variant_data(vcf, var="%DP")
 
@@ -137,7 +132,7 @@ ggplot(r, aes(x=AC, y = DP, alpha=0.01)) +
   labs(title="ALT allele count vs DP")
 
 ggsave("ALT_vs_DP.png")
-                 
+
 
 m <- sample_depth %>%
   group_by(mt) %>%
@@ -147,7 +142,7 @@ m <- sample_depth %>%
 
 mito <- m[2]/m[1]
 mito <- mito %>%
-        select(-contains("mt"), -contains("POS"), -contains("count"))
+  select(-contains("mt"), -contains("POS"), -contains("count"))
 
 # Reshape
 
@@ -155,10 +150,10 @@ mito <- mito %>%
 
 # Depth by Chromosome
 s <- group_by(sample_depth, CHROM) %>%
-     summarise_each(funs(mean))
+  summarise_each(funs(mean))
 
 
 
 # Plot ratios
 s <- ungroup(s) %>%
-     group_by(mt)
+  group_by(mt)
