ab85ca7ed555c51938c2b5750066840bafde9360
Daniel Cook <danielecook@gmail.com>
2015-08-25 14:09:00 -0500

    clean start

--- R/boxplot.R
+++
@@ -1,6 +0,0 @@
-ggplot(df) +
-  geom_boxplot(aes({aes}),{opts.geom}) +
-  labs(x="{xlab}", y="{ylab}", title="{opts.title}") +
-  theme(title=element_text(size=20), 
-        axis.title.x = element_text(size=15),
-        axis.title.y = element_text(size=15)) {opts.add}
