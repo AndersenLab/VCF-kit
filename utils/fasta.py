a9fdaa66a37bcfe77baeb4af3f0f0540744f03ed
Daniel Cook <danielecook@gmail.com>
2015-09-04 09:46:35 -0500

    Configured to parse fai

--- utils/fasta.py
+++ utils/fasta.py
@@ -1,6 +1,10 @@
 from subprocess import check_output
+from collections import OrderedDict
 
 class Fasta:
+    """
+        Extract regions from fasta files indexed with samtools
+    """
     def __init__(self, reference):
         self.reference = reference
 
@@ -17,6 +21,12 @@ class Fasta:
         self.chrom = chrom_pos
         return self
 
-    def keys(self):
+    def keys(self, weight = False):
         with open(self.reference + ".fai") as f:
-            return [x.split("\t")[0] for x in f.read().strip().split("\n")]
+            chrom_length = [x.strip().split("\t")[0:2] for x in f.readlines()]
+            chrom_length = [[x[0],int(x[1])] for x in chrom_length]
+            if weight == True:
+                genome_length = sum([x[1] for x in chrom_length])
+                return OrderedDict([x[0],1.0*x[1]/genome_length] for x in chrom_length)
+            else:
+                return OrderedDict(chrom_length)
