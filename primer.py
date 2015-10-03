2c2761e332ee85879b1677e272c12e7c5d6d082e
Daniel Cook <danielecook@gmail.com>
2015-10-03 17:14:05 -0500

    SNP-SNP - identifies sites now.

--- primer.py
+++ primer.py
@@ -2,6 +2,7 @@
 """
 usage:
   tb.py primer [--ref=<reference>] <vcf>
+  tb.py primer snpsnp [--ref=<reference>] <vcf>
 
 Example
 
@@ -58,12 +59,15 @@ class seq_vcf(vcf):
                     self.reference.alt_contig_names = dict(zip(self.contigs.keys(), self.reference.keys(),))
 
 
-    def extract(self):
+    def extract_ref_alt(self):
         for i in self:
-            window = 1000
-            start = i.POS - window + 1
-            end = i.POS + window
-            yield self.reference[i.CHROM][start:end]
+            if i.is_snp:
+                window = 1000
+                start = i.POS - window + 1
+                end = i.POS + window
+                ref = self.reference[i.CHROM][start:i.POS-1].seq + i.REF + self.reference[i.CHROM][i.POS+1:end].seq
+                alt = self.reference[i.CHROM][start:i.POS-1].seq + i.ALT[0] + self.reference[i.CHROM][i.POS+1:end].seq
+                yield Seq(ref, DNA_SET), Seq(alt, DNA_SET)
             
 
 
@@ -73,21 +77,20 @@ if __name__ == '__main__':
                   version='VCF-Toolbox v0.1',
                   argv = debug,
                   options_first=False)
-    
+    print args
     # Locate Reference
     v = seq_vcf(args["<vcf>"], args["--ref"])
-    for i in v.extract():
-        print i
-        exit()
 
-    search_results2 = AllEnzymes.search( Seq("tattgaaaaaaac", DNA_SET ))
-    search_results = AllEnzymes.search(  Seq("tattgaagtaaac", DNA_SET ))
-    sr2 = search_results2.items()
-    for i in search_results2:
-      print i.size, i.elucidate()
-    #print search_results2.items()
-    print dir(AllEnzymes)
-    print [(k,v) for k,v in search_results.iteritems() if len(v) > 0 and (k,v) not in sr2]
-    
+    if args["snpsnp"] == True:
+        for ref, alt in v.extract_ref_alt():
+            restriction_ref = dict(AllEnzymes.search(ref).items())
+            restriction_alt = dict(AllEnzymes.search(alt).items())
+            ref_sites = {k:v for k,v in restriction_ref.items() if len(v) > 0 and len(v) <= 3 and
+                                                                 restriction_ref[k] != restriction_alt[k] and
+                                                                 abs(len(restriction_ref[k]) - len(restriction_alt[k])) == 1}
+            alt_sites = {k:v for k,v in restriction_alt.items() if k in ref_sites.keys()}
+            print ref_sites, alt_sites
+
+
 
     x = seq_vcf(args["<vcf>"])
