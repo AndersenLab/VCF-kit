1bb662fdf3e2e72f0f018e07341abf45918a4dc3
Dan Cook <danielecook@gmail.com>
2015-10-07 10:28:44 -0500

    Added tests for vcf2tsv

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import pytest
+from subprocess import check_output
 
 
-def f():
-    raise SystemExit(1)
+def test_vcf2tsv_long():
+    out = check_output("python tb.py vcf2tsv long --print-header test.vcf.gz | head -n 20 | md5", shell = True).strip()
+    assert out == "d010d3707c2a1d8ab3613d35d5facc96"
 
-def test_mytest():
-    with pytest.raises(SystemExit):
-        f()
+def test_vcf2tsv_wide():
+    out = check_output("python tb.py vcf2tsv wide --print-header test.vcf.gz | head -n 20 | md5", shell = True).strip()
+    assert out == "99081d87fa3af617693daa8d9d46b82f"
