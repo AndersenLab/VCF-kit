363f2e7c4d00350ba2d2f03d9204ced521179450
Dan Cook <danielecook@gmail.com>
2015-10-23 13:31:25 -0500

    phylo --> create trees

--- phylo.py
+++ phylo.py
@@ -2,7 +2,7 @@
 """
 usage:
   tb.py phylo fasta <vcf>
-  tb.py phylo nexus <vcf>
+  tb.py phylo tree (nj|upgma) [--plot] <vcf>
 
 options:
   -h --help                   Show this screen.
@@ -11,10 +11,10 @@ options:
 """
 from docopt import docopt
 from utils.vcf import *
+from subprocess import Popen
 import sys
 from collections import defaultdict
-from clint.textui import colored, puts, indent
-from nexus import NexusWriter
+from clint.textui import colored, puts, indent, puts_err
 
 import os
 
@@ -30,7 +30,10 @@ if __name__ == '__main__':
     module_path = os.path.split(os.path.realpath(__file__))[0]
     v = vcf(args["<vcf>"])
     samples = v.samples
-    if args["fasta"]:
+    if args["fasta"] or args["tree"]:
+        """
+            Generate an aligned fasta from a VCF file.
+        """
         seqs = {}
         for sample in samples:
           seqs[sample] = []
@@ -40,15 +43,29 @@ if __name__ == '__main__':
             sample_gt = zip(samples,[x[-1] for x in non_missing])
             for sample, gt in sample_gt:
               seqs[sample].append(gt)
-        for sample, seq in seqs.items():
+        if not args["tree"]:
+          for sample, seq in seqs.items():
             print(">" + sample)
             print(''.join(seq))
-    elif args["nexus"]:
-        n = NexusWriter()
-        for line in v:
-            if line.is_snp:
-                non_missing = [x.replace(".","?") for x in line.gt_bases]
-                sample_gt = zip(samples,[x[-1] for x in non_missing])
-                for sample, gt in sample_gt:
-                    n.add(sample, "chrom", gt)
-        print n.make_nexus(interleave=True, charblock=True) 
+        elif args["tree"]:
+          """
+            Generate a phylogenetic tree using an aligned fasta with muscle.
+          """
+          fasta = ""
+          with indent(4):
+              puts_err(colored.blue("\nGenerating Fasta\n"))
+          for sample, seq in seqs.items():
+              fasta += ">" + sample + "\n" + ''.join(seq) + "\n"
+          tree_type = "upgma" # default is upgma
+          if args["nj"]:
+              tree_type = "neighborjoining"
+          with indent(4):
+              puts_err(colored.blue("\nGenerating " + tree_type + " Tree\n"))
+          tree, err = Popen(["muscle","-maketree","-in","-","-cluster",tree_type], stdin = PIPE, stdout = PIPE).communicate(input = fasta)
+          print(tree)
+          if args["--plot"]:
+              # R code for plotting here!
+              pass
+
+
+
