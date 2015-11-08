2df654cf3a764d21f40ed77c823ea1d144bbfe7c
Daniel Cook <danielecook@gmail.com>
2015-11-08 16:58:18 -0600

    fixing printing behavior with rename

--- tb/rename.py
+++ tb/rename.py
@@ -36,4 +36,4 @@ if __name__ == '__main__':
                             line[9+n] = replacement
             print '\t'.join(line)
         else:
-            print line,
+            print line.strip()
