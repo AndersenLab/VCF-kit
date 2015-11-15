70410e64bb8888ea2b3ebd00bc32fc02b1f8d0a4
Daniel Cook <danielecook@gmail.com>
2015-11-15 15:24:12 -0600

    update test

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from subprocess import check_output
 
 
 def test_vcf2tsv_long():
-    out = check_output("python tb.py vcf2tsv long --print-header test.vcf.gz | head -n 20 | md5", shell = True).strip()
+    out = check_output("tb vcf2tsv long --print-header test.vcf.gz | head -n 20 | md5", shell = True).strip()
     print(out)
     assert out == "d010d3707c2a1d8ab3613d35d5facc96"
 
 def test_vcf2tsv_wide():
-    out = check_output("python tb.py vcf2tsv wide --print-header test.vcf.gz | head -n 20 | md5", shell = True).strip()
+    out = check_output("tb vcf2tsv wide --print-header test.vcf.gz | head -n 20 | md5", shell = True).strip()
     print(out)
     assert out == "99081d87fa3af617693daa8d9d46b82f"
