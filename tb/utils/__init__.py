2be1ad01b91187d4736ba389a34dd6eb84322c5d
Dan Cook <danielecook@gmail.com>
2015-12-01 13:30:50 -0600

    add __init__ for utils and fcns

---
+++ tb/utils/__init__.py
@@ -0,0 +1,14 @@
+def boolify(s):
+    if s == 'True':
+        return True
+    if s == 'False':
+        return False
+    raise ValueError("huh?")
+
+def autoconvert(s):
+    for fn in (boolify, int, float):
+        try:
+            return fn(s)
+        except ValueError:
+            pass
+    return s
