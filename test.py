b1f7fd84480cf093ca32e4b449baf2547cb52c5a
Daniel Cook <danielecook@gmail.com>
2015-12-04 07:36:20 -0600

    add exceptions to test

--- test.py
+++ test.py
@@ -5,12 +5,16 @@ import hashlib
 
 def test_vcf2tsv_long():
     out, err = Popen(["tb","vcf2tsv","long","--print-header","test.vcf.gz"], stdout = PIPE).communicate()
+    if err:
+    	raise Exception(err)
     m = hashlib.md5()
     m.update(out)
     assert m.hexdigest() == "5d36ceaf5298e6c63b76cd13eefac647"
 
 def test_vcf2tsv_wide():
     out, err = Popen(["tb","vcf2tsv","wide","--print-header","test.vcf.gz"], stdout = PIPE).communicate()
+    if err:
+    	raise Exception(err)
     m = hashlib.md5()
     m.update(out)
     assert m.hexdigest() == "0fed86670ec7d2d2cae9b108b70d39d9"
