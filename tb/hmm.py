03234b62dd701e27ce6ae6d2eb5057f9d480d888
Dan Cook <danielecook@gmail.com>
2015-12-07 13:59:10 -0600

    vcf-out made much more efficient

--- tb/hmm.py
+++ tb/hmm.py
@@ -24,31 +24,31 @@ import itertools
 import numpy as np
 from pprint import pprint as pp
 from signal import signal, SIGPIPE, SIG_DFL
-signal(SIGPIPE,SIG_DFL) 
+signal(SIGPIPE, SIG_DFL)
 
 # Setup hmm
-model = Model( name="RIL_GT" )
+model = Model(name="RIL_GT")
 
-ref = State( DiscreteDistribution({ 'ref': 0.97, 'alt': 0.03 }) )
-alt = State( DiscreteDistribution({ 'ref': 0.03, 'alt': 0.97 }) )
+ref = State(DiscreteDistribution({'ref': 0.97, 'alt': 0.03}))
+alt = State(DiscreteDistribution({'ref': 0.03, 'alt': 0.97}))
 
-model.add_transition( model.start, ref, 0.5 )
-model.add_transition( model.start, alt, 0.5 )
+model.add_transition(model.start, ref, 0.5)
+model.add_transition(model.start, alt, 0.5)
 
 # Transition matrix, with 0.05 subtracted from each probability to add to
 # the probability of exiting the hmm
-model.add_transition( ref, ref, 0.9998 )
-model.add_transition( ref, alt, 0.0001 )
-model.add_transition( alt, ref, 0.0001 )
-model.add_transition( alt, alt, 0.9998 )
+model.add_transition(ref, ref, 0.9998)
+model.add_transition(ref, alt, 0.0001)
+model.add_transition(alt, ref, 0.0001)
+model.add_transition(alt, alt, 0.9998)
 
-model.add_transition( ref, model.end, 0.0001 )
-model.add_transition( alt, model.end, 0.0001 )
+model.add_transition(ref, model.end, 0.0001)
+model.add_transition(alt, model.end, 0.0001)
 
-model.bake( verbose=True )
+model.bake(verbose=True)
 
-to_model = {0:'ref', 1: 'alt'}
-to_gt = {0:'0/0', 1: '1/1'}
+to_model = {0: 'ref', 1: 'alt'}
+to_gt = {0: '0/0', 1: '1/1'}
 from_model = {True: 1, False: 0}
 
 debug = None
@@ -86,21 +86,26 @@ if __name__ == '__main__':
             positions.append(line.POS)
             gt_list.append(list(line.gt_types))
     gt_set = np.vstack([gt_list]).astype("int32")
-    gt_set[gt_set == 1] = -9 # Het
-    gt_set[gt_set == 2] = -9 # Missing
-    gt_set[gt_set == 3] = 1 # Alt
+    gt_set[gt_set == 1] = -9  # Het
+    gt_set[gt_set == 2] = -9  # Missing
+    gt_set[gt_set == 3] = 1  # Alt
     # Result dict
     vcf_gt_out = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+    sample_gt_matrix = np.array([])
+
+    chrom_to_factor = {y: x for x, y in enumerate(set(chromosome))}
+    sample_to_factor = {y: x for x, y in enumerate(v.samples)}
+    hmm_gt_set = []
     print_header = True
 
     for sample, column in zip(v.samples, gt_set.T):
         sample_gt = zip(chromosome, positions, column)
-        sample_gt = [x for x in sample_gt if x[2] in [0,1]]
+        sample_gt = [x for x in sample_gt if x[2] in [0, 1]]
         sequence = [to_model[x[2]] for x in sample_gt]
         if sequence:
-            results = model.forward_backward( sequence )[1]
-            result_gt = [from_model[x] for x in np.greater(results[:,0],results[:,1])]
-            results = ((a,b,c,d) for (a,b,c),d in zip(sample_gt, result_gt))
+            results = model.forward_backward(sequence)[1]
+            result_gt = [from_model[x] for x in np.greater(results[:, 0], results[:, 1])]
+            results = ((a, b, c, d) for (a, b, c), d in zip(sample_gt, result_gt))
             # Output results
             if args["--vcf-out"] is False:
                 if print_header:
@@ -114,7 +119,7 @@ if __name__ == '__main__':
                     site_count += 1
                     if chrom != last_contig or pred != last_pred:
                         if n > 0:
-                            out = '\t'.join(map(str,[contig, start, end, sample, last_pred+1, site_count]))
+                            out = '\t'.join(map(str, [contig, start, end, sample, last_pred + 1, site_count]))
                             print(out)
                             site_count = 0
                         contig = chrom
@@ -124,24 +129,26 @@ if __name__ == '__main__':
                     last_contig = chrom
                     last_pos = pos
                     n += 1
-                out = '\t'.join(map(str,[contig, start, end, sample, last_pred+1, site_count]))
+                out = '\t'.join(map(str, [contig, start, end, sample, last_pred + 1, site_count]))
                 print(out)
             elif args["--vcf-out"]:
+                out_list = []
                 for chrom, pos, orig, pred in results:
-                    vcf_gt_out[chrom][pos][sample] = {'orig': orig, 'pred' : pred}
-
+                    out_list.append([sample_to_factor[sample], chrom_to_factor[chrom], pos, orig, pred])
+                hmm_gt_set.append(np.vstack([out_list]).astype("int32"))
 
     if args["--vcf-out"]:
-        # Output vcf
+        hmm_gt = np.hstack([hmm_gt_set]).T
         v = vcf(args["<vcf>"])
-        print(v.insert_header_line("##FORMAT=<ID=GT_ORIG,Number=1,Type=String,Description=\"Original Genotype replaced by HMM\">"))
+        # print(v.insert_header_line("##FORMAT=<ID=GT_ORIG,Number=1,Type=String,Description=\"Original
+        # Genotype replaced by HMM\">"))
         for n, line in enumerate(v):
             line = variant_line(line)
             if line.has_gt and line.chrom in chromosome and line.pos in positions:
-                for s, sample in enumerate(v.samples):
-                    orig_pred = vcf_gt_out[line.chrom][line.pos][sample]
-                    if len(orig_pred) > 0:
-                        line.modify_gt_format(s, "GT", to_gt[orig_pred['pred']])
-                        line.modify_gt_format(s, "GT_ORIG", to_gt[orig_pred['orig']])
-                print(line)
-
+                for gt in hmm_gt:
+                    gt = gt[(chrom_to_factor[line.chrom] == gt[:, 1]) & (gt[:, 2] == line.pos)]
+                    if len(gt) > 0:
+                        sample_col, chrom, pos, orig, pred = gt[0]
+                        line.modify_gt_format(sample_col, "GT", to_gt[pred])
+                        line.modify_gt_format(sample_col, "GT_ORIG", to_gt[orig])
+                        print(line)
