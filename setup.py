4a833cbe6333b7b0ba0a5f48ee0bcd70d23b99cd
Daniel Cook <danielecook@gmail.com>
2016-08-21 19:06:09 -0500

    add code coverage

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import glob
 
-setup(name='vcf-toolbox',
+setup(name='vcf-kit',
       version='0.0.1',
       packages=['vk','vk.utils'],
       description='Tools for working with VCF files',
@@ -14,4 +14,6 @@ setup(name='vcf-toolbox',
       vk = vk.vk:main
       """,
       install_requires=["cython","docopt", "cyvcf2", "biopython", "clint", "requests"],
-      zip_safe=False)
+      zip_safe=False,
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'])
