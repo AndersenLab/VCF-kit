b50064dda984feb6828b1e5145a5539cedbb451d
Dan Cook <danielecook@gmail.com>
2016-11-11 11:35:19 -0600

    Fix freq test

--- tests/test_freq.py
+++ tests/test_freq.py
@@ -6,6 +6,6 @@ from test import Capturing, terminal
 def test_sample_hom_gt():
     with Capturing() as out:
         freq.main(["freq", "sample_hom_gt", "data/test.vcf.gz"])
-    out = out.splitlines()
+    out = str(out).splitlines()
     assert out[0] == 'sample\tfreq_of_gt\tn_gt_at_freq'
     assert out[10] == 'QG536\t10\t11'
