a0974f32c3820b391769d0c57da7aeac87e8d97b
Dan Cook <danielecook@gmail.com>
2016-11-11 13:17:49 -0600

    Fix freq tests

--- tests/test_freq.py
+++ tests/test_freq.py
@@ -8,3 +8,24 @@ def test_sample_hom_gt():
         freq.main(["freq", "sample_hom_gt", "data/test.vcf.gz"])
     assert out[0] == 'sample\tfreq_of_gt\tn_gt_at_freq'
     assert out[10] == 'QG536\t10\t11'
+
+
+
+def test_spectrum_genotypes_count():
+    with Capturing() as out:
+        freq.main(["freq", "genotypes", "data/test.vcf.gz"])
+    assert out[0] == 'n\tref\thet\talt\tmis'
+    assert out[-1] == '1\t9\t2\t1\t2'
+
+
+def test_spectrum_genotypes_frequency():
+    with Capturing() as out:
+        freq.main(["freq", "genotypes", "data/test.vcf.gz"])
+    assert out[-1] == '1\t9\t2\t1\t2'
+
+
+def test_spectrum_alleles_count():
+    with Capturing() as out:
+        freq.main(["freq", "spectrum", "data/test.vcf.gz"])
+    assert out[-2] == '23\t0.0357142857143'
+
