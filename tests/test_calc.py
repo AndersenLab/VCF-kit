d5d250060cd2373e4b490434592e51ac73402a13
Dan Cook <danielecook@gmail.com>
2016-11-11 13:39:09 -0600

    Finish calc rename

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,7 +5,7 @@ from test import Capturing, terminal
 
 def test_sample_hom_gt():
     with Capturing() as out:
-        freq.main(["calc", "sample_hom_gt", "data/test.vcf.gz"])
+        calc.main(["calc", "sample_hom_gt", "data/test.vcf.gz"])
     assert out[0] == 'sample\tfreq_of_gt\tn_gt_at_freq'
     assert out[10] == 'QG536\t10\t11'
 
@@ -13,19 +13,19 @@ def test_sample_hom_gt():
 
 def test_spectrum_genotypes_count():
     with Capturing() as out:
-        freq.main(["calc", "genotypes", "data/test.vcf.gz"])
+        calc.main(["calc", "genotypes", "data/test.vcf.gz"])
     assert out[0] == 'n\tref\thet\talt\tmis'
     assert out[-1] == '1\t9\t2\t1\t2'
 
 
 def test_spectrum_genotypes_frequency():
     with Capturing() as out:
-        freq.main(["calc", "genotypes", "data/test.vcf.gz"])
+        calc.main(["calc", "genotypes", "data/test.vcf.gz"])
     assert out[-1] == '1\t9\t2\t1\t2'
 
 
 def test_spectrum_alleles_count():
     with Capturing() as out:
-        freq.main(["calc", "spectrum", "data/test.vcf.gz"])
+        calc.main(["calc", "spectrum", "data/test.vcf.gz"])
     assert out[-2] == '23\t0.0357142857143'
 
