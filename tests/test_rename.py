2fa1fc8a30cbac65f9090962c402cc295d0baea1
Daniel Cook <danielecook@gmail.com>
2016-11-11 19:52:09 -0600

    modify rename so captured by coverage

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -2,30 +2,35 @@ from vcfkit import rename
 from cyvcf2 import VCF
 from subprocess import Popen, PIPE
 import hashlib
-from test_utilities import terminal
+from tests.test_utilities import terminal
 from cStringIO import StringIO
 
 
 def test_rename_prefix():
-    comm = "vk rename --prefix prefix data/test.vcf.gz | bcftools query --list-samples"
+    comm = "vk rename --prefix prefix_ data/test.vcf.gz | bcftools query --list-samples"
+    # Run 2x for testing
+    rename.main(["rename","--prefix","prefix_","data/test.vcf.gz"])
     out, err = Popen(comm, stdout=PIPE, stderr=PIPE, shell=True).communicate()
     assert all([x.startswith("prefix") for x in out.splitlines()])
 
 
 def test_rename_suffix():
-    comm = "vk rename --suffix data/test.vcf.gz | bcftools query --list-samples"
+    comm = "vk rename --suffix _suffix data/test.vcf.gz | bcftools query --list-samples"
+    rename.main(["rename","--suffix","_suffix","data/test.vcf.gz"])
     out, err = Popen(comm, stdout=PIPE, stderr=PIPE, shell=True).communicate()
-    assert all([x.endswith("suffix") for x in out.splitlines()])
+    assert all([x.endswith("_suffix") for x in out.splitlines()])
 
 
 def test_rename_subst():
     comm = "vk rename --subst N2:TEST --subst WN2001,TEST2 --subst AB1=TEST3 data/test.vcf.gz | bcftools query --list-samples"
+    rename.main(["rename","--subst","N2:TEST","--subst","WN2001,TEST2","--subst","AB1=TEST3", "data/test.vcf.gz"])
     out, err = Popen(comm, stdout=PIPE, stderr=PIPE, shell=True).communicate()
     assert sum([x.startswith("TEST") for x in out.splitlines()]) == 3
 
 
 def test_rename_combo():    
     comm = "vk rename --prefix PREFIX_ --suffix _SUFFIX --subst N2:TEST --subst WN2001,TEST2 --subst AB1=TEST3 data/test.vcf.gz | bcftools query --list-samples"
+    rename.main(["rename","--prefix","PREFIX_","--suffix","_SUFFIX","--subst","N2:TEST","--subst","WN2001,TEST2","--subst","AB1=TEST3","data/test.vcf.gz"])
     out, err = Popen(comm, stdout=PIPE, stderr=PIPE, shell=True).communicate()
     assert all([x.startswith("PREFIX_") for x in out.splitlines()])
     assert sum(["TEST" in x for x in out.splitlines()]) == 3
