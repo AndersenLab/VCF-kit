6a5e4b6d1d49592cfc1fffbc20e336d758b8ddf4
Daniel Cook <danielecook@gmail.com>
2016-11-11 20:30:09 -0600

    fix genome test

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -6,7 +6,7 @@ from os.path import expanduser
 from os import listdir
 import re
 from vcfkit import genome
-from test import terminal
+from test.test_utilities import terminal
 
 
 def test_genome_location():
