d961d6e943866c65a30eef32c05898251c88aff5
Daniel Cook <danielecook@gmail.com>
2017-02-25 18:59:24 -0600

    update phylo tests

--- tests/test_phylo.py
+++ tests/test_phylo.py
@@ -8,14 +8,14 @@ def test_phylo_fasta():
     with Capturing() as out:
         phylo.main(["phylo", "fasta", "test_data/test.vcf.gz"])
     h = hashlib.sha224(str(out)).hexdigest()
-    assert h == "81eda4b80e96eed31749e0a28d1acbd345fcd216023d17735978e999"
+    assert h == "3e13b81be200e1128d6ee52ff68139d84ac7d638bc3bf3d272241766"
 
 
 def test_phylo_nj():
     with Capturing() as out:
         phylo.main(["phylo", "tree", "nj", "test_data/test.vcf.gz"])
     h = hashlib.sha224(str(out)).hexdigest()
-    assert h == "96cdd2d7221bcbea8d86e099fdc1af02594d9dcad4daf8a5f85d0557"
+    assert h == "5faa8909fab5d5141e8232d1fb05705a01cd608cba9610d740166fff"
 
 
 def test_phylo_upgma():
@@ -23,7 +23,7 @@ def test_phylo_upgma():
         comm = ["phylo", "tree", "upgma", "test_data/test.vcf.gz"]
         phylo.main(comm)
     h = hashlib.sha224(str(out)).hexdigest()
-    assert h == "e3ef017c77bca5fd3feef7ed6215a37a5f0bb41101c125b3e493049e"
+    assert h == "4d0deed3dd8421ebf4c3f4458e027c7f2af7c4a699bfd8ce0f3067cf"
 
 
 def test_phylo_plot():
