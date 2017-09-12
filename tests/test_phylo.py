48adca86f8094cdb20bde8a20d9570a205efbf37
danielecook <danielecook@gmail.com>
2017-09-12 13:54:44 -0500

    Fix phylo tests

--- tests/test_phylo.py
+++ tests/test_phylo.py
@@ -15,13 +15,13 @@ def test_phylo_fasta():
 def test_phylo_nj():
     with Capturing() as out:
         phylo.main(["phylo", "tree", "nj", "test_data/test.vcf.gz"])
-    h = hashlib.sha224(''.join(out)).hexdigest()
-    assert h == "707c94f6edc7e5806f80cdb8ff02367cedb94d675ef1aafde19006ed"
+    line = out[24]
+    assert line == "DL200:0.132458"
 
 
 def test_phylo_upgma():
     with Capturing() as out:
         comm = ["phylo", "tree", "upgma", "test_data/test.vcf.gz"]
         phylo.main(comm)
-    h = hashlib.sha224(''.join(out)).hexdigest()
-    assert h == "142ece5d98398583dccb9ca4ecf76cc0dc5689a6beaa54721a72a785"
+    line = out[25]
+    assert line == "RC301:0.0854988"
