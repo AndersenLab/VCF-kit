e62d2308331712a165d82815667c24357e90595a
Daniel E. Cook <danielecook@gmail.com>
2020-08-20 17:06:38 -0400

    print genome files

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -30,6 +30,7 @@ def test_search_genome():
 def test_download_genome():
     genome.main(["genome", "ncbi", "--ref", "F1L3"])
     genome_files = [x for x in listdir(expanduser("~/.genome/F1L3")) if x != ".DS_Store"]
+    print(genome_files)
     assert len(genome_files) >= 11
 
 
