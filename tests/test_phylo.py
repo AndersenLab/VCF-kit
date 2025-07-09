54aaa624cd72280eb787c98081f1bf8a8528543c
msauria <mike.sauria@gmail.com>
2025-07-09 15:29:40 -0400

    Updated phylo tests

--- tests/test_phylo.py
+++ tests/test_phylo.py
@@ -8,23 +8,19 @@ def test_phylo_fasta():
     with Capturing() as out:
         phylo.main(["phylo", "fasta", "test_data/test.vcf.gz"])
     last_line = out[-1]
-    assert 2958 == len(last_line)
+    assert 2959 == len(last_line)
     assert last_line.startswith("AANNCTC")
 
-#######################
-# Disabled until replacement for "muscle -maketree" is found
-#######################
+
+def test_phylo_nj():
+    with Capturing() as out:
+        phylo.main(["phylo", "tree", "nj", "test_data/test.vcf.gz"])
+    line = out.split(',')[11]
+    assert line.startswith("DL200:0.1099")
 
 
-# def test_phylo_nj():
-#     with Capturing() as out:
-#         phylo.main(["phylo", "tree", "nj", "test_data/test.vcf.gz"])
-#     line = out[24]
-#     assert line.startswith("DL200:0.1324")
-
-
-# def test_phylo_upgma():
-#     with Capturing() as out:
-#         phylo.main(["phylo", "tree", "upgma", "test_data/test.vcf.gz"])
-#     line = out[25]
-#     assert line.startswith("RC301:0.085")
+def test_phylo_upgma():
+    with Capturing() as out:
+        phylo.main(["phylo", "tree", "upgma", "test_data/test.vcf.gz"])
+    line = out.split(',')[11]
+    assert line.startswith("DL200:0.1201")
